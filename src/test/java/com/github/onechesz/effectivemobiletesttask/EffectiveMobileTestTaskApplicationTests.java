3e0f9d285fe9edeae0b83129ea041446db05ddad
Ivan Minyaev <i.onechesz@gmail.com>
2023-08-17 15:58:44 +0300

    Initial commit

---
+++ src/test/java/com/github/onechesz/effectivemobiletesttask/EffectiveMobileTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.onechesz.effectivemobiletesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
